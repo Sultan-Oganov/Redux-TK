251959b3159b86926521cf4d6b518c8af1a7fb10
Sultan Oganov <sultan.oganov@gmail.com>
2022-12-13 23:01:36 +0600

    Redux TK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./redux/toolkitReducer";
+import './App.css'
 
-function App() {
+export default function App() {
+  const count = useSelector(state => state.toolkit.count);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="buttons-wrap">
+        <button onClick={() =>dispatch(increment())}>+</button>
+        <button onClick={() =>dispatch(decrement())}>-</button>
+      </div>
+      <h2>{count}</h2>
     </div>
   );
 }
-
-export default App;
---
+++ src/redux/toolkitReducer.js
@@ -0,0 +1,18 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0,
+    todos: ['abc', 'def', 'ghi', 'jkl', 'mno']
+}
+
+export const increment = createAction('INCREMENT');
+export const decrement = createAction('DECREMENT');
+
+export default createReducer(initialState, {
+    [increment]: (state) => {
+        state.count = state.count + 1;
+    },
+    [decrement]: (state) => {
+        state.count = state.count - 1;
+    }
+})
